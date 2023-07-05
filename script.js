61bece43a9a21dcb0e2f370d0dee1627cfe14d7d
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-07-05 18:04:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const li = document.getElementsByTagName("li");
+
+let ind = 0;
+for(let i=0; i<li.length; i++){
+  if(li[i].id === "level"){
+  ind = i;
+  break;
+    }
+}
+console.log(ind+1);
